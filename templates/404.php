24f61db3a65d98a1660f24292768ad0d68aa9c7e
Daria <volynchikovadaria@gmail.com>
2024-05-19 00:27:47 +0200

    +design for 404

--- templates/404.php
+++ templates/404.php
@@ -2,11 +2,10 @@
 include_once('partials/header.php');
 ?>
 
-  <section class="container error pt-3 mb-4">
-
-    <h1> 404 </h1>
-</div>
-
+  <div class="container error d-flex flex-column align-items-center justify-content-center">
+    <h1>404</h1>
+    <p class="text-center">Prepáčte, stránka, ktorú hľadáte, neexistuje.</p>
+  </div>
 
 <?php
 include_once('partials/footer.php')
